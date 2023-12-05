bd830c24bdfe49714fdd4c1cadf09f1db9c412ad
Mikolaj Izdebski <mizdebsk@redhat.com>
2023-12-05 16:51:35 +0100

    Fix gleaning module-info from multi-release JARs

--- src/main/java/org/fedoraproject/xmvn/generator/jpms/JPMSGenerator.java
+++ src/main/java/org/fedoraproject/xmvn/generator/jpms/JPMSGenerator.java
@@ -1,10 +1,11 @@
 package org.fedoraproject.xmvn.generator.jpms;
 
 import java.io.IOException;
-import java.nio.file.Files;
+import java.io.InputStream;
 import java.nio.file.Path;
+import java.util.Iterator;
 import java.util.jar.JarEntry;
-import java.util.jar.JarInputStream;
+import java.util.jar.JarFile;
 
 import org.fedoraproject.xmvn.generator.Collector;
 import org.fedoraproject.xmvn.generator.Generator;
@@ -14,11 +15,15 @@ class JPMSGenerator implements Generator {
     public void generate(Path filePath, Collector collector) {
         ManifestGleaner manifestGleaner = new ManifestGleaner(collector);
         ModuleInfoGleaner moduleInfoGleaner = new ModuleInfoGleaner(collector);
-        try (JarInputStream jarInputStream = new JarInputStream(Files.newInputStream(filePath))) {
-            manifestGleaner.glean(jarInputStream.getManifest());
-            for (JarEntry jarEntry; (jarEntry = jarInputStream.getNextJarEntry()) != null;) {
-                if ("module-info.class".equals(jarEntry.getRealName())) {
-                    moduleInfoGleaner.glean(jarInputStream);
+        try (JarFile jarFile = new JarFile(filePath.toFile(), false, JarFile.OPEN_READ, JarFile.runtimeVersion())) {
+            manifestGleaner.glean(jarFile.getManifest());
+            Iterator<JarEntry> it = jarFile.versionedStream().iterator();
+            while (it.hasNext()) {
+                JarEntry jarEntry = it.next();
+                if ("module-info.class".equals(jarEntry.getName())) {
+                    try (InputStream is = jarFile.getInputStream(jarEntry)) {
+                        moduleInfoGleaner.glean(is);
+                    }
                 }
             }
         } catch (IOException e) {
