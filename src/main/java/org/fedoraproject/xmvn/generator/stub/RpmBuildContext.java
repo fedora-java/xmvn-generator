93e3bef772c631a18fcb9fc07228a45a8e5455ed
Mikolaj Izdebski <mizdebsk@redhat.com>
2024-08-24 09:14:34 +0200

    Switch from JNI to FFM

--- src/main/java/org/fedoraproject/xmvn/generator/stub/RpmBuildContext.java
+++ src/main/java/org/fedoraproject/xmvn/generator/stub/RpmBuildContext.java
@@ -1,26 +1,52 @@
 package org.fedoraproject.xmvn.generator.stub;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import static java.lang.foreign.MemorySegment.NULL;
+import static java.lang.foreign.ValueLayout.ADDRESS;
+
+import java.lang.foreign.Arena;
+import java.lang.foreign.FunctionDescriptor;
+import java.lang.foreign.Linker;
+import java.lang.foreign.MemoryLayout;
+import java.lang.foreign.MemorySegment;
+import java.lang.foreign.ValueLayout;
+import java.lang.invoke.MethodHandle;
+import java.util.Optional;
 
 import org.fedoraproject.xmvn.generator.BuildContext;
 
 class RpmBuildContext implements BuildContext {
-    static {
-        try (InputStream is = RpmBuildContext.class.getResourceAsStream("/xmvn-generator-native.so")) {
-            Path p = Files.createTempFile("xmvngen-native", ".so");
-            try (OutputStream os = Files.newOutputStream(p)) {
-                is.transferTo(os);
-            }
-            System.load(p.toString());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    private static final ValueLayout STRING = ADDRESS
+            .withTargetLayout(MemoryLayout.sequenceLayout(Long.MAX_VALUE, ValueLayout.JAVA_BYTE));
+    private final MethodHandle rpmExpandMH;
+
+    public RpmBuildContext() {
+        Linker linker = Linker.nativeLinker();
+        Optional<MemorySegment> dlsymAddr = linker.defaultLookup().find("dlsym");
+        if (dlsymAddr.isEmpty()) {
+            throw new RuntimeException("dlsym is not available");
         }
+        FunctionDescriptor dlsymFD = FunctionDescriptor.of(ADDRESS, ADDRESS, STRING);
+        MethodHandle dlsymMH = linker.downcallHandle(dlsymAddr.get(), dlsymFD);
+        MemorySegment rpmExpandAddr;
+        try (Arena arena = Arena.ofConfined()) {
+            rpmExpandAddr = (MemorySegment) dlsymMH.invokeExact(NULL, arena.allocateFrom("rpmExpand"));
+        } catch (Throwable t) {
+            throw new RuntimeException("Unable to lookup rpmExpand symbol with dlsym()", t);
+        }
+        if (NULL.equals(rpmExpandAddr)) {
+            throw new RuntimeException(
+                    "Symbol rpmExpand was not found - dlsym(RTLD_DEFAULT, \"rpmExpand\") returned NULL");
+        }
+        FunctionDescriptor rpmExpandFD = FunctionDescriptor.of(STRING, STRING, ADDRESS);
+        rpmExpandMH = linker.downcallHandle(rpmExpandAddr, rpmExpandFD);
     }
 
     @Override
-    public native String eval(String macro);
+    public String eval(String macro) {
+        try (Arena arena = Arena.ofConfined()) {
+            return ((MemorySegment) rpmExpandMH.invokeExact(arena.allocateFrom(macro), NULL)).getString(0);
+        } catch (Throwable t) {
+            throw new RuntimeException("Failed to invoke librpmio function rpmExpand()", t);
+        }
+    }
 }
--- src/test/java/org/fedoraproject/xmvn/generator/stub/RpmBuildContextTest.java
+++ src/test/java/org/fedoraproject/xmvn/generator/stub/RpmBuildContextTest.java
@@ -2,11 +2,48 @@ package org.fedoraproject.xmvn.generator.stub;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.lang.foreign.Arena;
+import java.lang.foreign.FunctionDescriptor;
+import java.lang.foreign.Linker;
+import java.lang.foreign.MemoryLayout;
+import java.lang.foreign.ValueLayout;
+import java.lang.invoke.MethodHandle;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import org.fedoraproject.xmvn.generator.BuildContext;
 
 public class RpmBuildContextTest {
+    /**
+     * RpmBuildContext assumes that it is ran in the context of an rpmbuild process.
+     * This implies that RPM libraries such as librpmio.so are loaded in the running
+     * process and RPM library symbols can be loaded with dlsym(RTLD_DEFAULT, ...).
+     * This assumption is not met when running tests, so relevant RPM native
+     * libraries must be loaded explicitly before starting the actual test.
+     */
+    @BeforeAll
+    public static void loadNativeLibraries() {
+        Linker linker = Linker.nativeLinker();
+        MethodHandle dlopen = linker.downcallHandle(linker.defaultLookup().find("dlopen").get(),
+                FunctionDescriptor.ofVoid(
+                        ValueLayout.ADDRESS
+                                .withTargetLayout(MemoryLayout.sequenceLayout(Long.MAX_VALUE, ValueLayout.JAVA_BYTE)),
+                        ValueLayout.JAVA_INT));
+        // Try to dlopen() various libraries and ignore failures
+        for (String lib : List.of("librpm.so.10", "librpm.so.9")) {
+            try (Arena arena = Arena.ofConfined()) {
+                // From dlfcn.h
+                int RTLD_LAZY = 0x00001;
+                int RTLD_GLOBAL = 0x00100;
+                dlopen.invokeExact(arena.allocateFrom(lib), RTLD_LAZY | RTLD_GLOBAL);
+            } catch (Throwable t) {
+                throw new RuntimeException(t);
+            }
+        }
+    }
+
     @Test
     public void testEval() {
         BuildContext bc = new RpmBuildContext();
